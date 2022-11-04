b076eac9f8bad606ee0e8cc1cc7e2cc2fc656d5a
Rahul Yadav <yrahul9452@gmail.com>
2022-11-04 19:01:13 +0000

    Update App.js and Counter.jsx

--- src/Components/Counter.jsx
+++ src/Components/Counter.jsx
@@ -2,8 +2,8 @@ const Counter = ({ handleChange, count }) => {
   return (
     <div>
       <h1>counter:{count}</h1>
-      <button onclick={() => handleChange(1)}>ADD</button>
-      <button onclick={() => handleChange(-1)}>SUB</button>
+      <button onclick={() => handleChange()}>ADD</button>
+      <button onclick={() => handleChange()}>SUB</button>
     </div>
   );
 };
